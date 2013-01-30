48665494efe8728ab644063c51d89c300cf2e8da
Alexandre di Costanzo <adicosta@gmail.com>
2013-01-30 22:17:44 +0100

    Corporate pages

--- grails-app/controllers/thepieuvre/web/WelcomeController.groovy
+++ grails-app/controllers/thepieuvre/web/WelcomeController.groovy
@@ -6,6 +6,18 @@ import thepieuvre.core.FeedGlobalEnum
 
 class WelcomeController {
 
+	def about() {
+		render view: '/about'
+	}
+
+	def help() {
+		render view: '/help'
+	}
+
+	def contact() {
+		render view: '/contact'
+	}
+
 	def index = {
 		def articles = Article.createCriteria().list {
 			maxResults(25)
@@ -13,6 +25,26 @@ class WelcomeController {
 			feed { eq 'global', FeedGlobalEnum.GLOBAL } 
 		}
 		render view: '/index', model: ['articles': articles, 'tFeeds': Feed.count(), 'tArticles': Article.count()]
+	}
+
+	def executor = {
+		log.info "Executor: $params"
+		if (! params.command){
+			redirect action: 'index'
+			return true
+		}
+
+		def articles = Article.createCriteria().list {
+			maxResults(25)
+			order('dateCreated', 'desc')
+			feed { eq 'global', FeedGlobalEnum.GLOBAL }
+			ilike 'title', "%${params.command}%" 
+		}
+
+		render view: '/index', model: ['articles': articles,
+			'tFeeds': Feed.count(),
+			'tArticles': Article.count(),
+			'command': params.command]
 	}
 
 }
---
+++ grails-app/views/contact.gsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="layout" content="web"/>
+	</head>
+	<body>
+		<h1>Contact</h1>
+	</body>
+</html>
