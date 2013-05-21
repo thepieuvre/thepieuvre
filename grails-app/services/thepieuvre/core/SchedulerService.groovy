16772b08bc9f9530fd0eb55f45845af4d9dec7e1
Alexandre di Costanzo <adicosta@gmail.com>
2013-05-21 23:58:16 +0200

    Named thread and longer timeout on redis

--- grails-app/services/thepieuvre/core/SchedulerService.groovy
+++ grails-app/services/thepieuvre/core/SchedulerService.groovy
@@ -4,7 +4,7 @@ class SchedulerService {
 
 	static transactional = true
 
-	private static Timer scheduler = new Timer('pieuvre', true)
+	private static Timer scheduler = new Timer('pieuvre', false)
 
 	def schedule(TimerTask task, long period) {
 		scheduler.schedule(task, 31415, period)
