6715bcf37360c3011d9fcadfc2e6b489d81f9125
Alexandre di Costanzo <adicosta@gmail.com>
2013-05-22 12:47:28 +0200

    Starting thread in a service

--- grails-app/services/thepieuvre/core/SchedulerService.groovy
+++ grails-app/services/thepieuvre/core/SchedulerService.groovy
@@ -6,11 +6,21 @@ class SchedulerService {
 
 	private static Timer scheduler = new Timer('pieuvre', false)
 
+	def grailsApplication
+
 	def schedule(TimerTask task, long period) {
 		scheduler.schedule(task, 31415, period)
 	}
 
 	def cancel() {
 		scheduler.cancel()
+	}
+
+	def startFeedParser() {
+		new Thread(new FeedParser(grailsApplication), 'FeedParser').start()
+	}
+
+	def startArticleTask() {
+		new Thread(new ArticleTask(grailsApplication), 'ArticleTask').start()
 	}
 }
