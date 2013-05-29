633e21b463c5a11c261b91c347e090e2322fac0e
Alexandre di Costanzo <alex@thepieuvre.com>
2013-05-29 23:14:02 +0200

    Using a session for getting Content Domain
    [fixes #10]

--- src/groovy/thepieuvre/core/ArticleTask.groovy
+++ src/groovy/thepieuvre/core/ArticleTask.groovy
@@ -28,18 +28,27 @@ class ArticleTask implements Runnable {
 								grailsApplication.mainContext.articleService.updateNlp(decoded.nlp)
 							} else {
 								log.info "updating content: $decoded.content.id"
-								Content content = Content.get(decoded.content.id)
-								if (content) {
-									grailsApplication.mainContext.feedService.update(content, decoded.content)
-								} else {
-									log.warn "Cannot find content for $decoded.content.id -> $decoded"
+								Content.withNewSession { session ->
+								def query = Content.where {
+										id == decoded.content.id
+									}
+									Content content = query.find()
+									if (content) {
+										grailsApplication.mainContext.feedService.update(content, decoded.content)
+									} else {
+										log.warn "Cannot find content for $decoded.content.id -> $decoded"
+										// TODO push it to the next queue??
+										// log.info "Pushing $article.id to queue:queue:extractor"
+	  									// queuesService.enqueue(article.contents)
+	  									// TODO force re-extractor of empty fullcontent articles...
+									}
 								}
 							}
 						} else {
 							continue
 						}
 					} catch (Exception e) {
-						log.error "A problem occured while poping queue:article / $decoded", e
+						log.error "A problem occured while poping queue:article", e
 						continue
 					}
 				}
