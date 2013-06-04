55881e71f9b8ad6e0737693ba36f2dd9c9e6fe92
Alexandre di Costanzo <alex@thepieuvre.com>
2013-06-04 22:54:33 +0200

    Safer test...

--- grails-app/controllers/thepieuvre/web/WelcomeController.groovy
+++ grails-app/controllers/thepieuvre/web/WelcomeController.groovy
@@ -242,7 +242,7 @@ $message
 
 	def article = {
 		Article article = Article.get(params.id)
-		if (! article?.language.startsWith('en')) {
+		if (! article?.language?.startsWith('en')) {
 			flash.message = 'Sorry, the Pieuvre just started learning english. Other languages are not yet supported.'
 		} else if (! article) {
             flash.message = 'Sorry, the Pieuvre cannot find your article.'
