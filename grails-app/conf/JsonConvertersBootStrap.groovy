47a4ff7dc8acb8b438906554a7b467cb9512f11d
Alexandre di Costanzo <alex@thepieuvre.com>
2013-07-08 18:30:57 +0200

    Adding a no-lang by default
    [fixes #38]

--- grails-app/conf/JsonConvertersBootStrap.groovy
+++ grails-app/conf/JsonConvertersBootStrap.groovy
@@ -15,7 +15,7 @@ class JsonConvertersBootStrap {
 			json.link = a.link
 			json.published = a.published
 			json.title = a.title
-			json.language = a.language
+			json.language = (a.language)?:'no-lang'
 			//json.contents = a.contents.fullText
 			json.contents = {
 				def c = "<h1>${it.title}</h1>"
